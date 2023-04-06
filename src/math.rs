309fcb2eec62aa1413fca768f36da349b34df70b
bofh <123368+bofh@users.noreply.github.com>
2023-04-06 03:11:30 +0200

    Add ellipsoid fitting function

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,5 @@
 #![allow(unused)]
-use nalgebra::{Matrix3, Vector3};
+use nalgebra::{DMatrix, Matrix3, Matrix6, OVector, SymmetricEigen, Vector3, U3};
 
 /// Returns principal square root of the 3x3 matrix
 fn sqrt_m(matrix: &Matrix3<f64>) -> Matrix3<f64> {
@@ -25,4 +25,55 @@ pub fn ellipsoid_to_calibration(
     let b = n.dot(&(m_1 * n));
     let a_1 = (f / (b - d).sqrt()) * sqrt_m(&m);
     return (a_1, b);
+}
+
+/// Fits ellipsoid to set of points
+fn ellipsoid_fit(s: &Vec<[f64; 3]>) -> (Matrix3<f64>, Vector3<f64>, f64) {
+    let n = s.len();
+    let mut d = DMatrix::<f64>::zeros(10, n);
+    for j in 0..n {
+        let s_j = OVector::<f64, U3>::from_row_slice(&s[j]);
+        d[(0, j)] = s_j[0].powi(2);
+        d[(1, j)] = s_j[1].powi(2);
+        d[(2, j)] = s_j[2].powi(2);
+        d[(3, j)] = 2.0 * s_j[1] * s_j[2];
+        d[(4, j)] = 2.0 * s_j[0] * s_j[2];
+        d[(5, j)] = 2.0 * s_j[0] * s_j[1];
+        d[(6, j)] = 2.0 * s_j[0];
+        d[(7, j)] = 2.0 * s_j[1];
+        d[(8, j)] = 2.0 * s_j[2];
+        d[(9, j)] = 1.0;
+    }
+    let s = d.clone() * d.transpose();
+    let s_11 = s.fixed_view::<6, 6>(0, 0);
+    let s_12 = s.fixed_view::<6, 4>(0, 6);
+    let s_21 = s.fixed_view::<4, 6>(6, 0);
+    let s_22 = s.fixed_view::<4, 4>(6, 6);
+    let c = Matrix6::new(
+        -1., 1., 1., 0., 0., 0., 1., -1., 1., 0., 0., 0., 1., 1., -1., 0., 0., 0., 0., 0., 0., -4.,
+        0., 0., 0., 0., 0., 0., -4., 0., 0., 0., 0., 0., 0., -4.,
+    );
+    let inv_c = c.try_inverse().expect("C is not invertible");
+    let inv_s_22 = s_22.try_inverse().expect("S_22 is not invertible");
+
+    let e = inv_c * (s_11 - (s_12 * inv_s_22 * s_21));
+    let eigendec = SymmetricEigen::new(e);
+    let e_w = eigendec.eigenvalues;
+    let e_v = eigendec.eigenvectors;
+
+    let (argmax, _) = e_w.argmax();
+    let p_v_1 = e_v.column(argmax);
+    let v_1 = if p_v_1[0] < 0.0 {
+        -1. * p_v_1
+    } else {
+        1. * p_v_1
+    };
+
+    let v_2 = (-inv_s_22 * s_21) * v_1;
+    let m = Matrix3::new(
+        v_1[0], v_1[3], v_1[4], v_1[3], v_1[1], v_1[5], v_1[4], v_1[5], v_1[2],
+    );
+    let n = Vector3::new(v_2[0], v_2[1], v_2[2]);
+    let d = v_2[3];
+    (m, n, d)
 }
